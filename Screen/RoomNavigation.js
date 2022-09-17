335c5966aaa1e099019b8754f99004272002acc9
Pavan Ahirwar <91248254+Sharad8518@users.noreply.github.com>
2022-09-17 11:23:37 +0530

    transform

--- Screen/RoomNavigation.js
+++ Screen/RoomNavigation.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Easing } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Share from './Share';
 import Detail from './Detail';
-import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+import { TransitionSpecs } from 'react-navigation-stack';
+
 
   const Stack =  createNativeStackNavigator();
+
+
+  // const MySwitch = createAnimatedSwitchNavigator(
+  //   {
+  //     Home: HomeScreen,
+  //     Other: OtherScreen,
+  //   },
+  //   {
+  //     // The previous screen will slide to the bottom while the next screen will fade in
+  //     transition: (
+  //       <Transition.Together>
+  //         <Transition.Out
+  //           type="slide-bottom"
+  //           durationMs={400}
+  //           interpolation="easeIn"
+  //         />
+  //         <Transition.In type="fade" durationMs={500} />
+  //       </Transition.Together>
+  //     ),
+  //   }
+  // );
+
 
  const config ={
   
@@ -21,7 +44,52 @@ import { createSharedElementStackNavigator } from 'react-navigation-shared-eleme
     restSpeedThreshold:0.01 
   }
 
+
  }
+
+ const closeConfig ={
+  
+  animation: 'timing',
+  
+  config:{
+  
+    duration:200,
+    easing  :Easing.linear,
+
+  }
+
+
+ }
+
+const customTransition ={
+gestureEnabled:true,
+gestureDirection:"horizontal",
+transitionSec:{
+
+  open:TransitionSpecs.TransitionIOSSpec,
+  close:TransitionSpecs.TransitionIOSSpec,
+},
+
+cardStyleInterpolator:({current,next,layouts})=>{
+return{
+cardStyle:{
+  transform:[
+      {
+        translateX:current.progress.interpolate({
+
+            
+        })
+      }
+  ]
+}
+
+}
+
+
+}
+
+}
+
 
 
 export default function RoomNavigation() {
@@ -29,12 +97,14 @@ export default function RoomNavigation() {
     
   return (
     <NavigationContainer>
-    <Stack.Navigator  screenOptions={{ headerShown: false, gestureEnabled: true, gestureDirection: 'horizontal',
-     
-     transitionSpec:{
+    <Stack.Navigator  screenOptions={{  
+     gestureEnabled:true,
+     gestureDirection:"vertical",
+     transitionConfig :{
       open:config,
-      close:config
-     }
+      close:closeConfig,
+     },
+    
 
   }}  >
       <Stack.Screen name="Share" component={Share} />
